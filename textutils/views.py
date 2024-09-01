8e481db2e4d0bd5950f7a4d181a72bdf5fe4a689
BRP0106 <154300966+ravibaraiya0106@users.noreply.github.com>
2024-09-01 10:31:11 +0530

    commit

--- textutils/views.py
+++ textutils/views.py
@@ -3,14 +3,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def about(request):
-    return render(request, "about.html")
-
-
-def contact(request):
-    return render(request, "contact.html")
-
-
 def index(request):
     return render(request, "index.html")
 
